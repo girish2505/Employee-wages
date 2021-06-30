0e0702ba4f76d5abc2b7e551e9b76460d97e8304
Vayugundla Girish Guptha <girishvayugundla@gmail.com>
2021-06-30 14:37:28 +0530

    employee wage uc5

--- Employee wage/Program.cs
+++ Employee wage/Program.cs
@@ -4,36 +4,42 @@ namespace Employee_wage
 {
     class Program
     {
-        //initialzing constants
         public const int FULL_TIME = 1;
         public const int ABSENT = 0;
         public const int PART_TIME = 2;
         public const int WAGE_PER_HOUR = 20;
+        public const int WORKING_DAYS = 20;
 
         static void Main(string[] args)
         {
-            int wagesPerDay = 0;
+            int wagesForMonth = 0;
             int workingHours = 0;
+            int totalWorkingHours = 0;
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
             // intializing random class
-            Random rand = new Random(); 
-            int empInput = rand.Next(0, 3);
-            switch (empInput)
+            Random rand = new Random();
+            //creating for loop
+            for (int day = 1; day <= WORKING_DAYS; day++)
             {
-                case FULL_TIME:
-                    Console.WriteLine("Employee is Present Full Time");
-                    workingHours = 8;
-                    break;
-                case PART_TIME:
-                    Console.WriteLine("Employee is Present Part Time");
-                    workingHours = 4;
-                    break;
-                default:
-                    Console.WriteLine("Employee is Absent");
-                    break;
+                int empInput = rand.Next(0, 3);
+                switch (empInput)
+                {
+                    case FULL_TIME:
+                        workingHours = 8;
+                        break;
+                    case PART_TIME:
+                        workingHours = 4;
+                        break;
+                    default:
+                        workingHours = 0;
+                        break;
+                }
+                totalWorkingHours += workingHours;
+
             }
-            wagesPerDay = WAGE_PER_HOUR * workingHours;
-            Console.WriteLine("Employee wage per day is " + wagesPerDay);
+            //formula for calculating wagesForMonth
+            wagesForMonth = WAGE_PER_HOUR * totalWorkingHours;
+            Console.WriteLine("Employee wage per month is " + wagesForMonth);
             Console.Read();
         }
     }
