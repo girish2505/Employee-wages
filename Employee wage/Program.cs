2432fa5b06f875736043f84ab5c937b10938c4df
Vayugundla Girish Guptha <girishvayugundla@gmail.com>
2021-06-30 14:07:54 +0530

    employee wage uc1

---
+++ Employee wage/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Employee_wage
+{
+    class Program
+    {
+        public const int PRESENT = 1;
+        public const int ABSENT = 0;
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            Random rand = new Random(); // intializing random class
+            int empInput = rand.Next(0, 2);
+            if (empInput == PRESENT)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.Read();
+        }
+    }
+}
+
