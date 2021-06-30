83abfbc747f0e11dec97db797721a23cc34a38cf
Vayugundla Girish Guptha <girishvayugundla@gmail.com>
2021-06-30 14:32:10 +0530

    employee wage uc4

--- Employee wage/Program.cs
+++ Employee wage/Program.cs
@@ -4,22 +4,36 @@ namespace Employee_wage
 {
     class Program
     {
-        public const int PRESENT = 1;
+        //initialzing constants
+        public const int FULL_TIME = 1;
         public const int ABSENT = 0;
+        public const int PART_TIME = 2;
+        public const int WAGE_PER_HOUR = 20;
+
         static void Main(string[] args)
         {
-
+            int wagesPerDay = 0;
+            int workingHours = 0;
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
-            Random rand = new Random(); // intializing random class
-            int empInput = rand.Next(0, 2);
-            if (empInput == PRESENT)
+            // intializing random class
+            Random rand = new Random(); 
+            int empInput = rand.Next(0, 3);
+            switch (empInput)
             {
-                Console.WriteLine("Employee is Present");
+                case FULL_TIME:
+                    Console.WriteLine("Employee is Present Full Time");
+                    workingHours = 8;
+                    break;
+                case PART_TIME:
+                    Console.WriteLine("Employee is Present Part Time");
+                    workingHours = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-            }
+            wagesPerDay = WAGE_PER_HOUR * workingHours;
+            Console.WriteLine("Employee wage per day is " + wagesPerDay);
             Console.Read();
         }
     }
