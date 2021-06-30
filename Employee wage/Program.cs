47116d328d486bb8678e2a8a4641fae712117a88
Vayugundla Girish Guptha <girishvayugundla@gmail.com>
2021-06-30 14:43:12 +0530

    employee wage uc6

--- Employee wage/Program.cs
+++ Employee wage/Program.cs
@@ -8,34 +8,38 @@ namespace Employee_wage
         public const int ABSENT = 0;
         public const int PART_TIME = 2;
         public const int WAGE_PER_HOUR = 20;
-        public const int WORKING_DAYS = 20;
+        public const int WORKING_DAYS_PER_MONTH = 20;
+        public const int WORKING_HOURS_PER_MONTH = 100;
 
         static void Main(string[] args)
         {
+            int workingDays = 0;
             int wagesForMonth = 0;
             int workingHours = 0;
             int totalWorkingHours = 0;
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
-            // intializing random class
-            Random rand = new Random();
+            Random rand = new Random(); 
             //creating for loop
-            for (int day = 1; day <= WORKING_DAYS; day++)
+            while (totalWorkingHours <= WORKING_HOURS_PER_MONTH && workingDays < WORKING_DAYS_PER_MONTH)
             {
                 int empInput = rand.Next(0, 3);
                 switch (empInput)
                 {
                     case FULL_TIME:
+                        Console.WriteLine("Employee is Present Full Time");
                         workingHours = 8;
                         break;
                     case PART_TIME:
+                        Console.WriteLine("Employee is Present Part Time");
                         workingHours = 4;
                         break;
                     default:
+                        Console.WriteLine("Employee is Absent");
                         workingHours = 0;
                         break;
                 }
                 totalWorkingHours += workingHours;
-
+                workingDays++;
             }
             //formula for calculating wagesForMonth
             wagesForMonth = WAGE_PER_HOUR * totalWorkingHours;
