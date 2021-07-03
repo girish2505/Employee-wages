c3aa1800aa6bafb0d8e8052d430b31b262b0b660
Vayugundla Girish Guptha <girishvayugundla@gmail.com>
2021-07-04 00:01:38 +0530

    [Girish]Employee wages for multipul companies

--- Employee wage/Program.cs
+++ Employee wage/Program.cs
@@ -7,58 +7,51 @@ namespace Employee_wage
         public const int FULL_TIME = 1;
         public const int ABSENT = 0;
         public const int PART_TIME = 2;
-        public const int WAGE_PER_HOUR = 20;
-        public const int WORKING_DAYS_PER_MONTH = 20;
-        public const int WORKING_HOURS_PER_MONTH = 100;
+        //public const int WAGE_PER_HOUR = 20;
+        //public const int WORKING_DAYS_PER_MONTH = 20;
+        //public const int WORKING_HOURS_PER_MONTH = 100;
 
-        public static void ComputeWage()
+
+        private static void CalculateEmpWage(string company, int wagePerHour, int workingDaysPerMonth, int maxWorkingHours)
         {
-            int empHours = 0;
-            int empWage = 0;
             int workingDays = 0;
             int wagesForMonth = 0;
             int workingHours = 0;
             int totalWorkingHours = 0;
-            Console.WriteLine("Welcome to Employee Wage Computation Problem");
-            Random rand = new Random(); 
-            //creating for loop
-            while (totalWorkingHours <= WORKING_HOURS_PER_MONTH && workingDays < WORKING_DAYS_PER_MONTH)
+
+            Random rand = new Random(); // intializing random class
+            //for loop for calculating for 20 days
+            while (totalWorkingHours <= maxWorkingHours && workingDays < workingDaysPerMonth)
             {
-                int empInput = rand.Next(0, 3);
+                int empInput = rand.Next(0, 3);//Generating random b/w 0 and 3
                 switch (empInput)
                 {
                     case FULL_TIME:
-                       // Console.WriteLine("Employee is Present Full Time");
                         workingHours = 8;
                         break;
                     case PART_TIME:
-                        //Console.WriteLine("Employee is Present Part Time");
                         workingHours = 4;
                         break;
                     default:
-                       // Console.WriteLine("Employee is Absent");
                         workingHours = 0;
                         break;
                 }
-                //applying formula for finding out employee salary
-                empWage = WAGE_PER_HOUR * empHours;
-                workingHours += empHours;
                 totalWorkingHours += workingHours;
-                if (empInput != 0)
-                {
-                    workingDays++;
+                workingDays++;
 
-                }
             }
-            //formula for calculating wagesForMonth
-            wagesForMonth = WAGE_PER_HOUR * totalWorkingHours;
-            Console.WriteLine("Employee wage per month is " + wagesForMonth);
-            Console.Read();
+
+            wagesForMonth = wagePerHour * totalWorkingHours;//formula for wages
+            Console.WriteLine($"Total EmpWage For company {company} is {wagesForMonth}");
+
         }
+
         static void Main(string[] args)
         {
-            ComputeWage();
-
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            CalculateEmpWage("deloitte", 50, 20, 100);
+            CalculateEmpWage("microsoft", 40, 20, 200);
+            Console.Read();
         }
     }
 }
